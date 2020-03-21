3a46a9cf69910ae78f744d3f2b260a2098a062c5
e-caste-mbp <en.cas.dev@icloud.com>
2020-03-21 18:42:36 +0100

    remove TODO

--- boggle_telegram_bot.py
+++ boggle_telegram_bot.py
@@ -1175,7 +1175,6 @@ def main():
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # TODO: add greetings message when the bot gets added to a group
     # TODO: add init function that tells all players in games during bot's restart they have to begin the game again,
     #  and deletes all context.bot_data['games'] and context.chat_data current_game
     dp.add_handler(CommandHandler('start', start))
