c504bdc012708ca207978f5dde5cc04c890a4267
e-caste-mbp <en.cas.dev@icloud.com>
2020-04-15 15:21:37 +0200

    add notify_disable_notpresent

--- translations.py
+++ translations.py
@@ -533,6 +533,10 @@ translations = {
         'ita': "Sei già in attesa delle notifiche delle partite di questo gruppo, {}!",
         'eng': "You're already subscribed to the notifications for the next games, {}!"
     },
+    'notify_disable_notpresent': {
+        'ita': "Non ti sarebbero arrivate comunque le notifiche, {}, ma meglio stare sicuri.",
+        'eng': "You were already unsubscribed from all notifications, {}."
+    },
     'close_button': {
         'ita': "❌ Chiudi",
         'eng': "❌ Close"
